e6cf1f9d9eaaff093ae15b622e10fb8fb8012718
Sophia <spuertas@micds.org>
2018-06-21 14:57:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
+function appendDriver(name) { // fix this one
+  let newArray = drivers.push(name);
+  return newArray;
+} 
+function prependDriver(name) { //fix this one
+  let newArray = drivers.ushift(name);
+  return newArray;
+}
